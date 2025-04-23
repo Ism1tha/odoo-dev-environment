504422399b3e406c8ba85ef5169b5b84b8b8a8e6
Ism1tha <xgalan@hotmail.com>
2025-04-23 12:28:02 +0200

    fix: update summary and description in example addon manifest for clarity

--- addons/example-addon/__manifest__.py
+++ addons/example-addon/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Odoo example Addon",
-    "summary": "Example Addon for Odoo 18",
-    "description": "This is an example addon for Odoo 18. It demonstrates how to create a simple module with basic functionality.",
+    "summary": "Example Addon for Odoo",
+    "description": "This is an example addon for Odoo. It demonstrates how to create a simple module with basic functionality.",
     "author": "Ismael Semmar Galvez",
     "website": "https://github.com/Ism1tha/odoo-dev-environment",
     "version": "1.0",
