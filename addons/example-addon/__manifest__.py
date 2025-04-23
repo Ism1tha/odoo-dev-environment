451d696ba5623998d60ab14a81fb9d7eb0f7cf24
Ism1tha <xgalan@hotmail.com>
2025-04-23 12:10:47 +0200

    feat: add example addon for Odoo 18 with basic functionality

---
+++ addons/example-addon/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Odoo example Addon",
+    "summary": "Example Addon for Odoo 18",
+    "description": "This is an example addon for Odoo 18. It demonstrates how to create a simple module with basic functionality.",
+    "author": "Ismael Semmar Galvez",
+    "website": "https://github.com/Ism1tha/odoo-dev-environment",
+    "version": "1.0",
+    "license": "GPL-3",
+    "depends": ["base"],
+    "data": [
+    ],
+    "application": True,
+}
